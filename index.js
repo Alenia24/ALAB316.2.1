f633b6ab8b778f10ef17b1dc95c54e1e156aee2e
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-03-12 19:57:49 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let nameEl
+setTimeout(() => {
+  nameEl = prompt(`Enter your player name`);
+}, 5000);
+
+let imageEl = document.querySelector(".image");
+let titleEl = document.querySelector("#title");
+titleEl.classList.add("title");
+let guessEl = document.querySelector("#count");
+let number = Math.floor(Math.random() * 25 + 1);
+let guessCount = 5;
+let guess;
+setTimeout(() => {
+  guess = Number(prompt("Guess a number between 1 and 25!"));
+  checkGuess();
+}, 5000);
+
+function checkGuess() {
+  if (guess >= 1 && guess <= 25 && guess !== number && guessCount > 0) {
+    titleEl.innerHTML = `<h1>Wrong Guess ! 😂😂😂</h1>`;
+
+    setTimeout(tryAgain(), 500);
+  } else {
+    if (guess === number) {
+      titleEl.innerHTML = `<h1>You won ${nameEl}!</h1>`;
+      guessEl.innerHTML = `<h6>I lose! 😭😭😭</h6>`;
+      imageEl.innerHTML = `<img
+        class="img-fluid"
+        src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXM3Y3V5eXVyZmFvMmdqNGR5aHpta3QwOGVyYnAxc2RrdmthbTN2dyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/d3mlE7uhX8KFgEmY/giphy.gif"
+        alt=""
+        style="border-radius: 8px"
+      />`;
+    } else {
+      titleEl.innerHTML = `<h1>You loose ${nameEl}!</h1>`;
+      alert(`The number was ${number}`);
+      imageEl.innerHTML = `<img
+        class="img-fluid"
+        src="https://media.giphy.com/media/6ElPiQGynp3oAFPxij/giphy.gif?cid=790b7611401yihubsz6a5xxxz0oc2fq7oifpo2dg7yhyym4u&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+        alt=""
+        width=300
+        style="border-radius: 8px"
+      />`;
+    }
+  }
+}
+
+function tryAgain() {
+  if (guess > number) {
+    guessCount--;
+    guessEl.innerHTML = `<h6>You have ${guessCount} guess(es) remaining ${nameEl}!</h6>`;
+    setTimeout(() => {
+      guess = Number(
+        prompt("The number is lower!\nGuess a number between 1 and 25!")
+      );
+      checkGuess();
+    }, 500);
+  } else {
+    guessCount--;
+    guessEl.innerHTML = `<h6>You have ${guessCount} guess(es) remaining ${nameEl}!</h6>`;
+    setTimeout(() => {
+      guess = Number(
+        prompt("The number is higher!\nGuess a number between 1 and 25!")
+      );
+      checkGuess();
+    }, 500);
+  }
+}
